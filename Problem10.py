1fab2f3388da7c5fbf839e832b204f27faf5e3a1
Michael Jay Zamoras <thisiszammy@gmail.com>
2021-12-05 19:25:48 +0800

    Problem10

---
+++ Problem10.py
@@ -0,0 +1,86 @@
+def getMapBounds(coords):
+    vMax = -1
+    for i in range(len(coords)):
+        tMax = max(coords[i][0] + coords[i][1])
+        if  tMax > vMax:
+            vMax=tMax
+
+    return vMax
+
+coords = []
+x = "start"
+
+while True:
+    inp = input()
+
+    if inp == "end":
+        break
+
+    x = inp.split("->")
+        
+    coords.append([[int(a) for a in x[0].split(",")], [int (b) for b in x[1].split(",")]])
+
+
+mapBounds = getMapBounds(coords)
+gMap = [[0 for i in range(mapBounds+1)] for j in range(mapBounds+1)]
+ctr = 0
+
+for i in range(len(coords)):
+    sCoord = coords[i]
+
+    if sCoord[0][0] == sCoord[1][0]:
+        
+        if sCoord[1][1] > sCoord[0][1]:
+            for j in range(sCoord[0][1], sCoord[1][1]+1):
+                
+                gMap[sCoord[0][0]][j]+=1
+                if gMap[sCoord[0][0]][j] == 2:
+                    ctr+=1
+
+        elif sCoord[0][1] > sCoord[1][1]:
+            for j in range(sCoord[1][1], sCoord[0][1]+1):
+
+                gMap[sCoord[0][0]][j]+=1
+                if gMap[sCoord[0][0]][j] == 2:
+                    ctr+=1
+        else:
+            gMap[sCoord[0][0]][sCoord[0][1]]+=1
+            if gMap[sCoord[0][0]][sCoord[0][1]] == 2:
+                ctr+=1
+
+    elif sCoord[0][1] == sCoord[1][1]:
+        
+        if sCoord[1][0] > sCoord[0][0]:
+            for j in range(sCoord[0][0], sCoord[1][0]+1):
+                
+                gMap[j][sCoord[0][1]]+=1
+                if gMap[j][sCoord[0][1]] == 2:
+                    ctr+=1
+        
+        elif sCoord[0][0] > sCoord[1][0]:
+            for j in range(sCoord[1][0], sCoord[0][0]+1):
+
+                gMap[j][sCoord[0][1]]+=1
+                if gMap[j][sCoord[0][1]] == 2:
+                    ctr+=1
+        else:
+            gMap[sCoord[0][0]][sCoord[0][1]]+=1
+            if gMap[sCoord[0][0]][sCoord[0][1]] == 2:
+                ctr+=1
+    else:
+        slope = abs(sCoord[0][0]-sCoord[1][0])
+
+        xInc = 1 if sCoord[0][0] < sCoord[1][0] else -1
+        yInc = 1 if sCoord[0][1] < sCoord[1][1] else -1
+
+
+        for i in range(slope+1):
+            x = sCoord[0][0]+(i*xInc)
+            y = sCoord[0][1]+(i*yInc)
+
+            gMap[x][y]+=1
+            if gMap[x][y] == 2:
+                ctr+=1
+
+
+print(ctr)
